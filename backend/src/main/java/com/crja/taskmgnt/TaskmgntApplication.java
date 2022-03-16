2a50d0fb28da73e9c9162e48c17b402f5889e4e8
Marcio Barcellos <marcio.barcellos@outlook.com>
2022-03-16 15:08:20 -0300

    Projeto CRJA_API criado

---
+++ backend/src/main/java/com/crja/taskmgnt/TaskmgntApplication.java
@@ -0,0 +1,13 @@
+package com.crja.taskmgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmgntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmgntApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crja/taskmgnt/TaskmgntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crja.taskmgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmgntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

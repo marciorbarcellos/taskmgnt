b40d7c8163e1a9476eedfa5bf87e139ed6b8ead4
Marcio Barcellos <marcio.barcellos@outlook.com>
2022-03-16 19:53:36 -0300

    Configuração de segurança

--- frontend/src/components/pages/Form/index.tsx
+++ frontend/src/components/pages/Form/index.tsx
@@ -1,7 +1,45 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
 function Form() {
 
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
-        <h1>Página de Formulários</h1>
+        <div className="taskmgnt-form-container">
+            <img className="taskmgnt-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="taskmgnt-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="taskmgnt-form">
+                    <div className="form-group taskmgnt-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group taskmgnt-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="taskmgnt-form-btn-container">
+                        <button type="submit" className="btn btn-primary taskmgnt-btn">Salvar</button>
+                    </div>
+                </form >
+                <Link to="/">
+                    <button className="btn btn-primary taskmgnt-btn mt-3">Cancelar</button>
+                </Link>
+            </div >
+        </div >
     );
 }
 
--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,7 +1,51 @@
+import Pagination from "components/pagination";
+import { Link } from "react-router-dom";
+
 function Listing() {
 
     return (
-        <h1>Página de Lisiting</h1>
+        <>
+            <Pagination />
+
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Primeiro da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Segundo da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Terceiro da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Quarto da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Quinto da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                    <p>Sexto da Lista</p>
+                    <Link to={'/form/{movie.id}'}>
+                        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                    </Link>
+                </div>
+            </div>
+        </>
     );
 }
 
